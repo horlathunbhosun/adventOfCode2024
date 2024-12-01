66337954b39e8f2e03ce3e4fb5aad4711e85da9f
Olulode Olatunbosun <horlathunbhosun@gmail.com>
2024-12-01 13:42:53 +0100

    Feat: Day one of advent of code

---
+++ Day01_1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+var list1 = new List<int>();
+
+var list2 = new List<int>();
+
+var filePath = "/Users/olulodeolatunbosun/RiderProjects/AdventOfCode2024/Day01_1/input.txt";
+
+var lines = File.ReadAllLines(filePath);
+
+foreach (var line in lines )
+{
+    
+    var part = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    if (int.TryParse(part[0], out var number ))
+    {
+        list1.Add(number);
+    }
+    
+    if (int.TryParse(part[1], out var number2 ))
+    {
+        list2.Add(number2);
+    }
+    
+    list1.Sort();
+    list2.Sort();
+    
+    var addtionDiff = 0;
+    for (int i = 0; i < list1.Count; i++)
+    {
+        addtionDiff += Math.Abs(list1[i] - list2[i]);
+    }
+    Console.WriteLine(addtionDiff);
+}
---
+++ Day01_2/Program.cs
@@ -0,0 +1,41 @@
+﻿var list1 = new List<int>();
+var list2 = new List<int>();
+
+var count2 = new Dictionary<int, int>();
+
+var filePath = "/Users/olulodeolatunbosun/RiderProjects/AdventOfCode2024/Day01_2/input.txt";
+
+ var lines = File.ReadAllLines(filePath);
+
+
+foreach (var line in lines )
+{
+    
+    var part = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    if (int.TryParse(part[0], out var number ))
+    {
+        list1.Add(number);
+    }
+    
+    if (int.TryParse(part[1], out var number2 ))
+    {
+        list2.Add(number2);
+        
+        count2[number2] = count2.ContainsKey(number2) ? count2[number2] + 1 : 1;
+    }
+    
+    var addtion = 0;
+    
+    for (int i = 0; i < list1.Count; i++)
+    {
+        if (count2.ContainsKey(list1[i]))
+        {
+            addtion += list1[i] * count2[list1[i]];
+        }
+    }
+    
+    
+    Console.WriteLine(addtion);
+    //Console.ReadKey();
+
+}
