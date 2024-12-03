8341d86be6373340c19343c505c26940a5f22415
Olulode Olatunbosun <horlathunbhosun@gmail.com>
2024-12-03 10:25:44 +0100

    Feat: Day three

---
+++ Day03_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+var filePath = "/Users/olulodeolatunbosun/RiderProjects/AdventOfCode2024/Day03_01/input.txt";
+
+var lines = File.ReadAllLines(filePath);
+
+var result = 0;
+
+var regex = new Regex(@"mul\((\d+),(\d+)\)");
+
+foreach (var line in lines)
+{
+    //Console.WriteLine(line);
+
+    var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    var matches = regex.Matches(line);
+    foreach (Match match in matches)
+    {
+        if (int.TryParse(match.Groups[1].Value, out var num1 ) && int.TryParse(match.Groups[2].Value, out var num2))
+        {
+            result += num1 * num2;
+        }
+        
+    }
+}
+Console.WriteLine(result);
---
+++ Day03_02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+var filePath = "/Users/olulodeolatunbosun/RiderProjects/AdventOfCode2024/Day03_02/input.txt";
+
+var input = File.ReadAllText(filePath);
+
+string pattern = @"mul\((\d+),(\d+)\)|do\(\)|don't\(\)";
+var matches = Regex.Matches(input, pattern);
+bool mulEnabled = true; 
+int totalSum = 0;
+foreach (Match match in matches)
+{
+    if (match.Groups[0].Value == "do()")
+    {
+        mulEnabled = true;
+    }
+    else if (match.Groups[0].Value == "don't()")
+    {
+        mulEnabled = false;
+    }
+    else if (int.TryParse(match.Groups[1].Value, out var num1 ) && int.TryParse(match.Groups[2].Value, out var num2))
+    {
+        if (mulEnabled)
+        {
+            totalSum += num1 * num2;
+        }
+    }
+}
+
+Console.WriteLine(totalSum);    
+
+
+
+
+
+
