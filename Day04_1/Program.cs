4cff05b36928e04ed8b7d30183e09792ac2717fc
Olulode Olatunbosun <horlathunbhosun@gmail.com>
2024-12-04 10:43:59 +0100

    Feat: Day 4 part one

--- Day04_1/Program.cs
+++ Day04_1/Program.cs
@@ -1,44 +1,4 @@
-﻿// --- Day 4: Ceres Search ---
-//     "Looks like the Chief's not here. Next!" One of The Historians pulls out a device and pushes the only button on it. After a brief flash, you recognize the interior of the Ceres monitoring station!
-//
-//     As the search for the Chief continues, a small Elf who lives on the station tugs on your shirt; she'd like to know if you could help her with her word search (your puzzle input). She only has to find one word: XMAS.
-//
-//     This word search allows words to be horizontal, vertical, diagonal, written backwards, or even overlapping other words. It's a little unusual, though, as you don't merely need to find one instance of XMAS - you need to find all of them. Here are a few ways XMAS might appear, where irrelevant characters have been replaced with .:
-//
-//
-// ..X...
-//     .SAMX.
-//     .A..A.
-//     XMAS.S
-//     .X....
-//     The actual word search will be full of letters instead. For example:
-//
-// MMMSXXMASM
-//     MSAMXMSMSA
-// AMXSXMAAMM
-//     MSAMASMSMX
-// XMASAMXAMM
-//     XXAMMXXAMA
-// SMSMSASXSS
-//     SAXAMASAAA
-// MAMMMXMMMM
-//     MXMXAXMASX
-// In this word search, XMAS occurs a total of 18 times; here's the same word search again, but where letters not involved in any XMAS have been replaced with .:
-//
-// ....XXMAS.
-//     .SAMXMS...
-// ...S..A...
-// ..A.A.MS.X
-// XMASAMX.MM
-// X.....XA.A
-// S.S.S.S.SS
-//         .A.A.A.A.A
-//     ..M.M.M.MM
-//         .X.X.XMASX
-// Take a look at the little Elf's word search. How many times does XMAS appear?
-
-
-using System;
+﻿using System;
 using System.IO;
 using System.Linq;
 
@@ -88,20 +48,3 @@ for (int i = 0; i < lines.Length; i++)
 }
 
 Console.WriteLine(result);
-
-// for (int i = 0; i < lines.Length; i++)
-// {
-//     var line = lines[i];
-//     for (int j = 0; j < line.Length; j++)
-//     {
-//         if (line[j] == 'X')
-//         {
-//             if (j + 4 < line.Length && line[j + 1] == 'M' && line[j + 2] == 'A' && line[j + 3] == 'S')
-//             {
-//                 result++;
-//             }
-//         }
-//     }
-// }
-//
-// Console.WriteLine(result);
